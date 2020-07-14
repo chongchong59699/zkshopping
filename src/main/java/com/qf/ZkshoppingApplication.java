75901343647d5f9afa65ec245b9085de6e3ca2be
chongchong59699 <596995854@qq.com>
2020-07-14 16:17:27 +0800

    Initial commit

---
+++ src/main/java/com/qf/ZkshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkshoppingApplication.class, args);
+    }
+
+}
