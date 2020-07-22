8e4aa2bfa9a09e6378809c591a9dd61d8ecf870a
chongchong59699 <596995854@qq.com>
2020-07-22 21:37:15 +0800

    [修复了一些蠢的bug--git版本的问题]

--- src/main/java/com/qf/controller/ReceiverAddressInfoController.java
+++ src/main/java/com/qf/controller/ReceiverAddressInfoController.java
@@ -6,10 +6,8 @@ import com.qf.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @CrossOrigin
 @Api(tags = "收货地址管理")
 @RestController
