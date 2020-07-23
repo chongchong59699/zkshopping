fb0c5f99788d16fbc5ed59a19d6cb5cec400c429
zhaohy <824416729@qq.com>
2020-07-23 22:18:40 +0800

    [daily]zhaohongyu:search模块更改，添加搜索全站商品，搜索店铺需求

--- src/main/java/com/qf/dao/SearchDao.java
+++ src/main/java/com/qf/dao/SearchDao.java
@@ -5,15 +5,24 @@ import com.qf.dto.SearchGoodsInStoreDto;
 import com.qf.pojo.Goods;
 import com.qf.pojo.Store;
 import com.qf.vo.R;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SearchDao {
+    @Select("select store_id,logo_img_url from zkwg.store")
     List<BandDto> getAllBand();
 
+    @Select("select * from zkwg.store where store_name like concat('%',#{storeName},'%')")
     Store getStoreByName(String storeName);
 
     List<Goods> selectGoodsInStoreByKey(SearchGoodsInStoreDto searchGoodsInStoreDto);
+
+    @Select("select * from zkwg.goods where zkwg.goods.name like concat('%',#{key},'%')")
+    List<Goods> selectGoodsByName(String key);
+
+    @Select("select * from zkwg.store where zkwg.store.store_name like concat('%',#{key},'%')")
+    List<Store> selectStoresByName(String key);
 }
