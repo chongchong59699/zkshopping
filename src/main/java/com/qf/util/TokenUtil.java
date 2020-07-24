59127dfa6494b5a72ba0974941c196363f825d1e
renxiaoyu <1525452238@qq.com>
2020-07-24 14:20:58 +0800

    [添加tokenutil]

--- src/main/java/com/qf/util/TokenUtil.java
+++ src/main/java/com/qf/util/TokenUtil.java
@@ -1,5 +1,10 @@
 package com.qf.util;
 
+import com.alibaba.fastjson.JSON;
+import com.qf.config.RedisKeyConfig;
+import com.qf.pojo.User;
+import com.qf.vo.R;
+
 import java.util.UUID;
 
 /**
@@ -11,5 +16,13 @@ import java.util.UUID;
 public class TokenUtil {
     public static String createToken(int uid){
         return UUID.randomUUID().toString().replace("-","")+"_"+uid;
+    }
+    public static User getUserFromToken(String token,JedisCore jedisCore){
+        if(jedisCore.get(RedisKeyConfig.TOKEN_USER + token)!=null){
+            User user= JSON.parseObject(jedisCore.get(RedisKeyConfig.TOKEN_USER + token),User.class);
+            return user;
+        }else {
+            return null;
+        }
     }
 }
